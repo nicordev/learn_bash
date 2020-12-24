d76e003dde482d80ce26f388bdcee05d87042bce
Nicolas Renvoisé <n.renvoise@ubitransport.com>
2020-12-24 10:48:10 +0100

    improve variables

--- function/function_and_variables.sh
+++ function/function_and_variables.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
+SCRIPT_NAME=$(basename $0)
+styleNormal="\e[0m"
+styleStrong="\e[1m"
+styleSuccess="\e[32m"
+styleCaution="\e[33m"
+styleFailure="\e[31m"
+
 doStuff() {
+    echo -e "${styleStrong}Here we are in the function:${styleNormal} ${FUNCNAME[0]}"
     echo 'zog'
     echo 'zog'
 }
 
-echo -e "Calling doStuff..."
+echo -e "${styleStrong}We are in the script called ${SCRIPT_NAME}${styleNormal}"
+
+echo -e "${styleStrong}Calling function doStuff...${styleNormal}"
 
 doStuff
 
 zog=$(doStuff)
 
-echo -e "\necho \$zog..."
+echo -e "\n${styleStrong}echo \$zog${styleNormal}"
 echo $zog
 
-echo -e "\necho \"\$zog\"..."
+echo -e "\n${styleStrong}echo \"\$zog\"${styleNormal}"
 echo "$zog"
