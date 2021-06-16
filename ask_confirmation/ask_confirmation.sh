ea69a763e901afb60d491825c35bba648f6ab5c7
Nicolas Renvoisé <n.renvoise@ubitransport.com>
2021-06-16 12:15:34 +0200

    add ask_confirmation

---
+++ ask_confirmation/ask_confirmation.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Using read: https://www.computerhope.com/unix/bash/read.htm
+# -p show a prompt
+# -r use "raw input". Specifically, this option causes read to interpret backslashes literally, rather than interpreting them as escape characters.
+
+askConfirmationDefaultYes() {
+    echo -e "\e[1mContinue?\e[0m [YES/no] "
+    read answer
+
+    if [[ ${answer,,} =~ ^(n|no)$ ]]; then
+        echo "no"
+        return 1
+    fi
+
+    echo "yes"
+}
+
+askConfirmationDefaultNo() {
+    echo -e "\e[1mContinue?\e[0m [yes/NO] "
+    read answer
+
+    if [[ ${answer,,} =~ ^(y|yes)$ ]]; then
+        echo "yes"
+        return 1
+    fi
+
+    echo "no"
+}
+
+while true; do
+    read -p "How old are you? " age
+    askConfirmationDefaultYes || exit
+done
