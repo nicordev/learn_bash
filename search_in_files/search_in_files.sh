48e2ce8a6670f9d9771c19956815a0ecc2fff628
Nicolas Renvoisé <n.renvoise@ubitransport.com>
2021-08-30 12:58:46 +0200

    add search_in_files

--- search_in_files/search_in_files.sh
+++ search_in_files/search_in_files.sh
@@ -2,11 +2,21 @@
 
 search() {
     if [ $# -lt 2 ]; then
-        echo -e "$(basename $0) \e[33mstringToSearchHere directoryHere\e[0m"
+        echo -e "$(basename $0) \e[33mstringToSearchHere directoryHere fileInclusionPattern fileExclusionPattern\e[0m"
         exit 1
     fi
 
-    grep --line-number --dereference-recursive "$@"
+    if [ -n "$3" ]; then
+        local include="--include=$3"
+    fi
+
+    if [ -n "$4" ]; then
+        local exclude="--exclude=$4"
+    fi
+
+    echo $include $exclude
+
+    grep --line-number --dereference-recursive "$1" "$2" $include $exclude
 }
 
 search "$@"
