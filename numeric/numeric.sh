b69a328ba5f3b522b33a0bc345ba57e6ccf4d1ee
Nicolas Renvoisé <n.renvoise@ubitransport.com>
2021-09-02 17:25:05 +0200

    improve numeric

--- numeric/numeric.sh
+++ numeric/numeric.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+SCRIPT_NAME=$(basename $BASH_SOURCE)
+SCRIPT_DIRECTORY=$(dirname $BASH_SOURCE)
+
 demoIncrement() {
     i=0
 
@@ -18,6 +21,35 @@ demoIncrement() {
     let "i = i + 1"
 
     echo $i
+}
+
+decrement() {
+    if [ $# -lt 2 ];then
+        echo -e "${SCRIPT_NAME} ${FUNCNAME[0]} \e[33mmax count\e[0m"
+    fi
+
+    local i="$1"
+    local count="$2"
+    local limit=$(($i - $count))
+
+    echo "$i - $count = $limit"
+
+    while [ $i -gt $limit ]
+    do
+        echo $i
+        ((i--))
+    done
+
+}
+
+power() {
+    if [ $# -lt 2 ];then
+        echo -e "${SCRIPT_NAME} ${FUNCNAME[0]} \e[33mnumber power\e[0m"
+
+        return
+    fi
+
+    echo $(($1 ** $2))
 }
 
 _listAvailableFunctions() {
