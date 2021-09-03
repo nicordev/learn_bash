e14488f145a137cfb436f5e7a8b5fece209cb627
Nicolas Renvoisé <n.renvoise@ubitransport.com>
2021-09-03 10:43:50 +0200

    improve loop

--- loop/loop_through_files.sh
+++ loop/loop_through_files.sh
@@ -1,21 +1,20 @@
 #! /bin/bash
 
 loopThroughFiles() {
-    folder="$1"
-    ls "${folder}" | while read -r file; do # -r	do not allow backslashes to escape any characters
-        [[ "$file" =~ ^([0-9-]{3})([0-9]{4}_)bulletin_(de_paie) ]]
-        echo ${BASH_REMATCH[@]}
+    directory="$1"
+
+    shopt -s nullglob
+    for file in "$directory"/*; do
+        echo $file
     done
 }
 
-renameFiles() {
-    ls . | while read -r file; do # -r	do not allow backslashes to escape any characters
-        if _captureDateDigits $file
-        then
-            renamedFileName="${BASH_REMATCH[2]}-${BASH_REMATCH[1]}_${BASH_REMATCH[3]}"
-            echo ${file} ${renamedFileName}
-        fi
-    done
-}
+loopThroughFiles '.'
 
+echo '
+Full path:'
 loopThroughFiles $(pwd)
+
+echo '
+One liner:'
+for file in *; do echo "$file"; done
