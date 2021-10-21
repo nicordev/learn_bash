a14fcaee50226dcc90ce6ea0c70fca30437242e1
Nicolas Renvoisé <n.renvoise@ubitransport.com>
2021-10-21 16:52:02 +0200

    improve split_string

--- split_string/split_string.sh
+++ split_string/split_string.sh
@@ -15,6 +15,14 @@ do
     echo $fruit
 done
 
+# use an array
+fruitsBasket=( $someFruits )
+
+for fruit in ${fruitsBasket[@]}
+do
+    echo $fruit
+done
+
 # Set the IFS as it was initially
 IFS=$initialIfs
 
@@ -22,3 +30,16 @@ for fruit in $someFruits
 do
     echo $fruit
 done
+
+myString="hello:world"
+firstWord=${myString%:*}
+lastWord=${myString##*:}
+
+echo $firstWord $lastWord
+
+# be careful if the string does not have the separator: lastWord == firstWord
+myString="hello"
+firstWord=${myString%:*}
+lastWord=${myString##*:}
+
+echo $firstWord $lastWord
