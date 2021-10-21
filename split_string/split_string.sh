ad344751c1d959419ac8871afd3ec88d3a26c712
Nicolas Renvoisé <n.renvoise@ubitransport.com>
2021-10-21 11:38:07 +0200

    add split string

---
+++ split_string/split_string.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+echo "Hello${IFS}World!"
+
+someFruits="apple,orange,banana"
+
+# Remember the initial IFS
+initialIfs=$IFS
+
+# Set the IFS to the character we need to use to split
+IFS=','
+
+for fruit in $someFruits
+do
+    echo $fruit
+done
+
+# Set the IFS as it was initially
+IFS=$initialIfs
+
+for fruit in $someFruits
+do
+    echo $fruit
+done
